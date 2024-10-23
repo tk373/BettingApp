798a86d37ef42fbb07631f739772ecc2d2f33270
Silvan H. <108238800+tk373@users.noreply.github.com>
2024-10-23 16:45:37 +0200

    API Keys removed lol

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -8,15 +8,7 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAheB7Xi0WDTJYMsJ7l8EqBPtKiBkzhZIM",
-  authDomain: "modul-vapp.firebaseapp.com",
-  projectId: "modul-vapp",
-  storageBucket: "modul-vapp.appspot.com",
-  messagingSenderId: "720725084594",
-  appId: "1:720725084594:web:8e2f83162bc180b19f7cab",
-  measurementId: "G-098K24ZERS"
-};
+//add your firebase config here
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
